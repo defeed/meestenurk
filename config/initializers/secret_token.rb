1a8f42253f3ccb3e5827b42fa2cc0be52beb795e
Artem Pakk <tema@tema.ee>
2012-05-11 17:38:13 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meestenurk::Application.config.secret_token = '6b4931a57c1d7f5cdc94b6a41955473a7b0bbd8310d94cc028b6b8a7ac1a0993a2620e9d9b0bf23ad9b1a515bd04067da1e94df0d737abe5610e780992739e23'
